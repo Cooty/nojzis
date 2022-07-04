959bd14c4e3687e3efe89933437aaa423a4e7d60
Tamas Kuti <cooty13@gmail.com>
2022-07-04 12:45:36 +0200

    fix: add multiple formats dynamically

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,13 +1,23 @@
 <template>
     <div style="display: none" aria-hidden="true">
         <audio ref="audioTag" @ended="onEnded" @play="onPlay">
+            <source
+                v-if="fileNameToPlay"
+                :src="`media/${fileNameToPlay}.ogg`"
+                type="audio/ogg"
+            />
+            <source
+                v-if="fileNameToPlay"
+                :src="`media/${fileNameToPlay}.mp3`"
+                type="audio/mp3"
+            />
             Your browser does not support the audio element.
         </audio>
     </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, nextTick } from "vue";
 import { useSoundStore, usePlayerStore } from "../store";
 
 export default {
@@ -18,28 +28,23 @@ export default {
         const soundStore = useSoundStore();
         const playerStore = usePlayerStore();
 
-        soundStore.$subscribe(async (mutation, state) => {
-            console.log("sound in store has changed");
-            console.log(mutation);
-            console.log(state);
+        soundStore.$subscribe(async (_, state) => {
             if (state.soundToPlay === undefined) {
                 return;
             }
-            audioTag.value.src = `media/${state.soundToPlay}.ogg`;
-            audioTag.value.type = "audio/ogg";
+            fileNameToPlay.value = state.soundToPlay;
+            await nextTick();
+
             audioTag.value.load();
             audioTag.value.play();
         });
 
         const onEnded = () => {
-            console.log("play has ended");
-            // audioTag.value.pause();
             playerStore.isPlaying = false;
             soundStore.$reset();
         };
 
         const onPlay = () => {
-            console.log("playing");
             playerStore.isPlaying = true;
         };
 
